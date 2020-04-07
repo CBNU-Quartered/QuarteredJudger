1c42a7586e44567d90cbd3c080267ee344c30014
hyogeon <men1219@hanmail.net>
2020-04-08 01:23:43 +0900

    refactor: JudgeService change to kotlin

---
+++ src/main/java/judger/judge/JudgeService.kt
@@ -0,0 +1,51 @@
+package judger.judge
+
+import judger.judge.util.ShellCommandUtil
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.FileWriter
+import java.io.IOException
+
+@Service
+class JudgeService(@Autowired private val shellCommandProperties: ShellCommandProperties) {
+
+    fun run(source: String, language: String, input: String, answer: String): String {
+        createInputFile(input)
+
+        ShellCommandUtil.execCommand(shellCommandProperties.localInitCommand)
+        createSourceFile(source)
+        ShellCommandUtil.execCommand(shellCommandProperties.cCompileCommand)
+        ShellCommandUtil.execCommand(shellCommandProperties.cRunCommand)
+
+        return checkAnswer(answer)!!
+    }
+
+    @Throws(IOException::class)
+    private fun createSourceFile(source: String) {
+        val fileWriter = FileWriter(shellCommandProperties.testerDir + "/test.c")
+        fileWriter.write(source)
+        fileWriter.close()
+    }
+
+    @Throws(IOException::class)
+    private fun createInputFile(input: String) {
+        val fileWriter = FileWriter(shellCommandProperties.testerDir + "/input.txt")
+        fileWriter.write(input)
+        fileWriter.close()
+    }
+
+    @Throws(IOException::class)
+    private fun checkAnswer(answer: String): String? {
+        val br = BufferedReader(FileReader(shellCommandProperties.testerDir + "/output.txt"))
+        var output = ""
+        var temp: String?
+        while (br.readLine().also { temp = it } != null) output += temp
+
+        if (output == answer) {
+            return "CORRECT ANSWER !!"
+        }
+        return "WRONG"
+    }
+}
--- src/test/java/judger/judge/JudgeServiceTest.java
+++ src/test/java/judger/judge/JudgeServiceTest.java
@@ -1,12 +1,20 @@
 package judger.judge;
 
-import judger.judge.JudgeService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 class JudgeServiceTest {
+    @Autowired
+    private JudgeService judgeService;
+
     private static final String TEST_C = "#include<stdio.h>\n" +
             "int main(){\n" +
             "\tint a, b;\n" +
@@ -18,7 +26,6 @@ class JudgeServiceTest {
     @Test
     @DisplayName("C 코드 채점 테스트")
     void name() throws Exception {
-        JudgeService judgeService = new JudgeService();
         assertThat(judgeService.run(TEST_C, "C", "1 2", "3")).contains("CORRECT");
     }
 }
